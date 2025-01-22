c53237039ec649399ed975833ed7db6036c7f42c
elmedinarifi <elmedin.arifi@student.nhlstenden.com>
2025-01-22 16:37:50 +0100

    new-files

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "index.html",
+    "vergadertechnieken.html",
+    "professional_skills_semester1.html",
+    "portfolio.html",
+    "Vergader_agenda.pdf",
+    "Notulen_Plenair.docx",
+    "EvaluatieStudent_ElmedinArifi.docx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
